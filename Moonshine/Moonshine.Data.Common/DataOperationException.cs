5cd633321ab101a8cde049188c6645bfce82a7e5
patrick0xf <patrick0xf@thunderground.net>
2015-05-02 19:12:32 -0700

    Adding Common Project

---
+++ Moonshine/Moonshine.Data.Common/DataOperationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Moonshine.Data.Common
+{
+    public class DataOperationException : Exception
+    {
+        public DataOperationException(string message) : base(message)
+        {
+        }
+    }
+}
